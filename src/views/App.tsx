67dea1bbe10f554c84f906924797548b723053a6
xrei <romanrei.dev@gmail.com>
2019-09-20 18:22:13 +0300

    add router & styled

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Layout = styled.main`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  background-color: #212121;
+  color: #fff;
+`
 
 const App: React.FC = () => (
-  <div className="App">
-  </div>
+  <Layout />
 )
 
 export default App
