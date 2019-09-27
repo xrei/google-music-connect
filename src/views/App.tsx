f35ebfe54131635339f8f788238b6ed4cbc74e51
xrei <romanrei.dev@gmail.com>
2019-09-27 19:13:57 +0300

    add effector

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -5,12 +5,10 @@ const Layout = styled.main`
   display: flex;
   width: 100%;
   height: 100vh;
-  background-color: #212121;
+  background-color: #fffdf2;
   color: #fff;
 `
 
-const App: React.FC = () => (
+export const App: React.FC = () => (
   <Layout />
 )
-
-export default App
