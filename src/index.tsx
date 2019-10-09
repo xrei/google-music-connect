212507fad3979ee6b8637722917268bcb7a3acf6
xrei <romanrei.dev@gmail.com>
2019-10-10 00:55:35 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
-import './index.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { StylesProvider, ThemeProvider } from '@material-ui/styles'
+import './index.css'
 import theme from './theme'
 import {App} from './App'
 import * as serviceWorker from './serviceWorker'
@@ -11,7 +11,7 @@ ReactDOM.render(
   <StylesProvider injectFirst>
     <ThemeProvider theme={theme}>
       <Router>
-        <App />
+        <Route path='/' component={App}/>
       </Router>
     </ThemeProvider>
   </StylesProvider>
